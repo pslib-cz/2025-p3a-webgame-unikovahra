4d6d3490f1a413a3ffb3137c0e1de2deb8658cea
Jakub Jezbera <jakub.jezbera.023@pslib.cz>
2025-12-10 20:09:47 +0100

    pridani controlleru

---
+++ unikovahra.Server/Controllers/MiniGamesController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using unikovahra.Server.Data;
+
+namespace unikovahra.Server.Controllers
+{
+
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MiniGamesController : ControllerBase
+    {
+
+        private readonly AppDbContext _db;
+        public MiniGamesController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet("room/{roomId}")]
+        public IActionResult GetByRoom(int roomId)
+        {
+            var games = _db.MiniGames
+                .Where(g => g.RoomId == roomId)
+                .ToList();
+
+            return Ok(games);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var game = _db.MiniGames.Find(id);
+            if (game == null)
+            {
+                return NotFound();
+            }
+            return Ok(game);
+        }
+
+
+    }
+}
---
+++ unikovahra.Server/Controllers/RoomController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using unikovahra.Server.Data;
+
+namespace unikovahra.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class RoomController : ControllerBase
+    {
+        private readonly AppDbContext _db;
+
+        public RoomController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var rooms = _db.Rooms
+                .OrderBy(r => r.Order)
+                .ToList();
+            return Ok(rooms);
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var room = _db.Rooms.Find(id);
+            if (room == null)
+            {
+                return NotFound();
+            }
+            return Ok(room);
+        }
+
+
+
+
+    }
+}
