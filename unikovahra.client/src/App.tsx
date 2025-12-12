bc094d7a56544c6b9cbd67bcc2df662e7375c005
Kubaejj <jakub.jezbera.023@pslib.cz>
2025-12-12 16:20:13 +0100

    Fetcher - GamebookPage

--- unikovahra.client/src/App.tsx
+++ unikovahra.client/src/App.tsx
@@ -2,7 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import RootLayout from "./pages/RootLayout";
 import StartPage from "./pages/StartPage";
 
-import GamebookPage, {gamebookLoader} from "./pages/GamebookPage";
+import GamebookPage from "./pages/GamebookPage";
 import SwitchboardPage from "./pages/SwitchboardPage";
 import TabletPage from "./pages/TabletPage";
 import HackingPage from "./pages/HackingPage";
@@ -12,21 +12,19 @@ import MoneyGrabPage from "./pages/MoneyGrabPage";
 import FinishPage from "./pages/FinishPage";
 import NotFoundPage from "./pages/NotFoundPage";
 
-import MinigameInitialPage, {minigameLoader} from "./pages/MinigameInitialPage";
+import MinigameInitialPage from "./pages/MinigameInitialPage";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<RootLayout />}>
-        <Route index element={<StartPage />} />
+      <Route index element={<StartPage />} />
 
-        <Route path="gamebook/:id" element={<GamebookPage />} loader={gamebookLoader} errorElement={<NotFoundPage />} />
+      <Route path="gamebook/:id?" element={<GamebookPage />} />
 
 
-        <Route path="minigame" element={<MinigameInitialPage />} loader={minigameLoader} errorElement={<NotFoundPage />} />
-        {/*  jeste jedna stranka jako pravidla pro kazdou minihru? */ }
-
-        <Route path="minigame/switchboard" element={<SwitchboardPage />} />
+      <Route path="minigame" element={<MinigameInitialPage />} />
+      {/*  jeste jedna stranka jako pravidla pro kazdou minihru? */ }        <Route path="minigame/switchboard" element={<SwitchboardPage />} />
         <Route path="minigame/tablet" element={<TabletPage />} />
         <Route path="minigame/hacking" element={<HackingPage />} />
         <Route path="minigame/keylock" element={<KeylockPage />} />
