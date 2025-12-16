c5e65afbdcf5401bd0c19b442b216080bcd595b6
tomprochy77 <tomas.prochazka.023@pslib.cz>
2025-12-16 12:57:04 +0100

    remake db

--- unikovahra.Server/Models/MiniGame.cs
+++ unikovahra.Server/Models/MiniGame.cs
@@ -7,5 +7,13 @@
         public int RoomId { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
+
+        public string SuccessTitle { get; set; }
+        public string SuccessSubtitle { get; set; }
+        public string SuccessText { get; set; }
+
+        public string FailureTitle { get; set; }
+        public string FailureSubtitle { get; set; }
+        public string FailureText { get; set; }
     }
 }
